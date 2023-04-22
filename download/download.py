656a714dbf67d171b75135432e75bfbdfe703208
Jack Kwan <kwan6@purdue.edu>
2023-04-22 14:54:14 -0400

    Modify to accept npjms packages

--- download/download.py
+++ download/download.py
@@ -5,6 +5,7 @@ Date of last modification: 4/22/2023
 Function: download.py serves as the backend functionality 
           to provide users the ability to download packages from the database
 '''
+import re
 import sys
 import urllib.request
 import zipfile
@@ -12,17 +13,33 @@ import pymysql
 import sqlalchemy
 from google.cloud.sql.connector import Connector
 
+class BadRequest(Exception):
+    '''Define bad request exception'''
+    
 def download(repo_link):
     '''Functionality to download a repo as a zip'''
-    split_url = repo_link.split("/")
-    username = split_url[-2]
-    repo = split_url[-1].split(".")[0]
-    zip_url = f"https://github.com/{username}/{repo}/archive/refs/heads/main.zip"
-    zip_filename = f"{repo}.zip"
-    urllib.request.urlretrieve(zip_url, zip_filename)
-    with zipfile.ZipFile(zip_filename, "r") as zip_ref:
-        zip_ref.extractall(repo)
-        return 1
+    if "github.com" in repo_link:
+        match = re.match(r'https://github.com/([\w-]+)/([\w-]+)', repo_link)
+        if not match:
+            raise BadRequest('Invalid repository URL')
+        owner = match.group(1)
+        repo_name = match.group(2)
+        zip_url = f"https://github.com/{owner}/{repo_name}/archive/refs/heads/main.zip"
+        zip_filename = f"{repo_name}.zip"
+        urllib.request.urlretrieve(zip_url, zip_filename)
+        with zipfile.ZipFile(zip_filename, "r") as zip_ref:
+            zip_ref.extractall(repo_name)
+            return 1
+    elif "npmjs.com" in repo_link:
+        package_name = repo_link.split("/")[-1]
+        zip_url = f"https://registry.npmjs.org/{package_name}/-/{package_name}-latest.tgz"
+        zip_filename = f"{package_name}.tgz"
+        urllib.request.urlretrieve(zip_url, zip_filename)
+        with zipfile.ZipFile(zip_filename, "r") as zip_ref:
+            zip_ref.extractall(package_name)
+            return 1
+    else:
+        return 0        #Download failed
 
 connector = Connector()
 
