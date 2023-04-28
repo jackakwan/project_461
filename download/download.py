1be99a25f26513201c963e20c9927599f3594ce8
Jack Kwan <kwan6@purdue.edu>
2023-04-28 10:51:48 -0400

    Change to handle differently named main branches

--- download/download.py
+++ download/download.py
@@ -6,6 +6,7 @@ Function: download.py serves as the backend functionality
           to provide users the ability to download packages from the database
 '''
 import re
+import requests
 import sys
 import urllib.request
 import zipfile
@@ -16,6 +17,21 @@ from google.cloud.sql.connector import Connector
 class BadRequest(Exception):
     '''Define bad request exception'''
     
+def getGithubURLs(repo):
+    '''Transform npm links to github links'''
+    webUrl = urllib.request.urlopen(repo)
+    if webUrl.getcode() == 200:
+        html_cont = webUrl.read().decode("utf-8")
+        r1 = r'<span id="repository-link">(.*?)<\/span>'
+        try:
+            reg_out = re.search(r1, html_cont)
+            gitLink = "https://" + reg_out.group(1)
+        except:
+            raise Exception("Valid GitHub link not found.\n")
+    else:
+        raise Exception("npm url not able to connect.\n")
+    return gitLink   
+ 
 def download(repo_link):
     '''Functionality to download a repo as a zip'''
     if "github.com" in repo_link:
@@ -25,18 +41,39 @@ def download(repo_link):
         owner = match.group(1)
         repo_name = match.group(2)
         zip_url = f"https://github.com/{owner}/{repo_name}/archive/refs/heads/main.zip"
-        zip_filename = f"{repo_name}.zip"
-        urllib.request.urlretrieve(zip_url, zip_filename)
-        with zipfile.ZipFile(zip_filename, "r") as zip_ref:
-            zip_ref.extractall(repo_name)
+        # Send a GET request to the URL and retrieve the response content
+        response = requests.get(zip_url)
+        zip_content = response.content
+        if response.status_code == 404:
+            print('master not main')
+            zip_url = f"https://github.com/{owner}/{repo_name}/archive/refs/heads/master.zip"
+            response = requests.get(zip_url)
+            zip_content = response.content
+        # Create a new file in binary write mode and write the zip content to it
+        repo_name = repo_name.rstrip('/')
+        with open(repo_name + ".zip", "wb") as f:
+            f.write(zip_content)
             return 1
     elif "npmjs.com" in repo_link:
-        package_name = repo_link.split("/")[-1]
-        zip_url = f"https://registry.npmjs.org/{package_name}/-/{package_name}-latest.tgz"
-        zip_filename = f"{package_name}.tgz"
-        urllib.request.urlretrieve(zip_url, zip_filename)
-        with zipfile.ZipFile(zip_filename, "r") as zip_ref:
-            zip_ref.extractall(package_name)
+        repo_link = getGithubURLs(repo_link)
+        match = re.match(r'https://github.com/([\w-]+)/([\w-]+)', repo_link)
+        if not match:
+            raise BadRequest('Invalid repository URL')
+        owner = match.group(1)
+        repo_name = match.group(2)
+        zip_url = f"https://github.com/{owner}/{repo_name}/archive/refs/heads/main.zip"
+        # Send a GET request to the URL and retrieve the response content
+        response = requests.get(zip_url)
+        zip_content = response.content
+        if response.status_code == 404:
+            print('master not main')
+            zip_url = f"https://github.com/{owner}/{repo_name}/archive/refs/heads/master.zip"
+            response = requests.get(zip_url)
+            zip_content = response.content
+        # Create a new file in binary write mode and write the zip content to it
+        repo_name = repo_name.rstrip('/')
+        with open(repo_name + ".zip", "wb") as f:
+            f.write(zip_content)
             return 1
     else:
         return 0        #Download failed
